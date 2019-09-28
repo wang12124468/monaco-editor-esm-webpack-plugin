aa729635167bc1e8436e14d30c5544d71ef4f05c
xiaoyong <454926339@qq.com>
2019-09-28 15:17:47 +0800

    s

--- src/core.js
+++ src/core.js
@@ -1,18 +1,24 @@
 const MonacoEditorWebapckPlugin = require('monaco-editor-webpack-plugin');
 const NormalModuleWebpackReplacementPlugin = require("webpack/lib/NormalModuleReplacementPlugin");
-const nls = require.resolve('monaco-editor-nls/nls.js');
+const nls = require.resolve('monaco-editor-nls');
 
-function MonacoEditorEsmWebpackPlugin({ isMonacoEditorWebapckPlugin = true, ...options }) {
+function MonacoEditorEsmWebpackPlugin({ isMonacoEditorWebapckPlugin = true, isReplaceNls = true, ...options } = {}) {
     if(isMonacoEditorWebapckPlugin) {
         this.plugin1 = new MonacoEditorWebapckPlugin(options);
     }
-    this.plugin2 = new NormalModuleWebpackReplacementPlugin(/\/(vscode\-)?nls\.js/, function(resource) {
-        resource.request = nls;
-        resource.resource = nls;
-    });
+    if(isReplaceNls) {
+        this.plugin2 = new NormalModuleWebpackReplacementPlugin(/\/(vscode\-)?nls\.js/, function(resource) {
+            resource.request = nls;
+            resource.resource = nls;
+        });
+    }
 }
 
 MonacoEditorEsmWebpackPlugin.prototype.apply = function(compiler) {
     this.plugin1 && this.plugin1.apply(compiler);
-    this.plugin2.apply(compiler);
+    this.plugin2 && this.plugin2.apply(compiler);
 }
+
+MonacoEditorEsmWebpackPlugin.loader = require.resolve('./loader');
+
+module.exports = MonacoEditorEsmWebpackPlugin;
--- src/loader.js
+++ src/loader.js
@@ -1,10 +1,10 @@
 module.exports = function(content, map, meta) {
-    if(/monaco-editor[\/\\]esm[\/\\]vs.+\.js$/.test(this.resourcePath)) {
-        const vsPath = this.resourcePath.split(/monaco-editor[\/\\]esm/).pop();
+    if(/monaco-editor[\\\/]esm[\\\/]vs.+\.js$/.test(this.resourcePath)) {
+        const vsPath = this.resourcePath.split(/monaco-editor[\\\/]esm[\\\/]/).pop();
         if(vsPath) {
             // localize\.apply\(\s*([^,]+)\s*,\s*\
             const path = vsPath.replace(/\\/g, '/').replace('.js', '');
-            return content.replace(/localize\(/g, `localize(${path}, `);
+            return content.replace(/localize\(/g, `localize('${path}', `);
         }
     }
     return content;
