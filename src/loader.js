8dd2a1c6363aceb738623ca8f55e987bc2634df8
xiaoyong <454926339@qq.com>
2022-04-21 10:09:11 +0800

    fix the loader error in worker that has compile with nls.js in monaco-editor

--- src/loader.js
+++ src/loader.js
@@ -2,9 +2,14 @@ module.exports = function(content, map, meta) {
     if(/monaco-editor[\\\/]esm[\\\/]vs.+\.js$/.test(this.resourcePath)) {
         const vsPath = this.resourcePath.split(/monaco-editor[\\\/]esm[\\\/]/).pop();
         if(vsPath) {
-            // localize\.apply\(\s*([^,]+)\s*,\s*\
             const path = vsPath.replace(/\\/g, '/').replace('.js', '');
-            return content.replace(/localize\(/g, `localize('${path}', `);
+            // return content.replace(/localize\(/g, `localize('${path}', `);
+            return content.replace(/(\bfunction\s+localize\()|(\blocalize\()/g, function(text) {
+                if(/function\s+localize/.test(text)) {
+                    return text;
+                }
+                return `localize('${path}', `;
+            });
         }
     }
     return content;
